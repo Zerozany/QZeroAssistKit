31726d260d1317b115d4caa09c5773d13d511930
Zerozany <691139128@qq.com>
2026-04-22 13:28:10 +0800

    feat:修改了部分代码的格式化排版

--- src/ViewEngineBase/ViewEngineBase.cpp
+++ src/ViewEngineBase/ViewEngineBase.cpp
@@ -9,7 +9,8 @@ ViewEngineBase::ViewEngineBase(QQmlApplicationEngine& _qmlApplicationEngine, QOb
 auto ViewEngineBase::connectSignal2Slot() noexcept -> void
 {
     connect(&m_qmlApplicationEngine, &QQmlApplicationEngine::objectCreationFailed, qApp, [] { QCoreApplication::exit(-1); }, Qt::QueuedConnection);
-    connect(&m_qmlApplicationEngine, &QQmlApplicationEngine::warnings, qApp, [](const QList<QQmlError>& _warnings) {   for (const auto &warning : _warnings) {
-        qDebug() << warning.toString();
+    connect(&m_qmlApplicationEngine, &QQmlApplicationEngine::warnings, qApp, [](const QList<QQmlError>& _warnings) {   
+        for (const auto &warning : _warnings) {
+            qDebug() << warning.toString();
     } }, Qt::QueuedConnection);
 }
