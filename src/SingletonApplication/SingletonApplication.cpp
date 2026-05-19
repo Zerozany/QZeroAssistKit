2fa19395b59d4eb53279ce9473494d1bbadc5932
Zerozany <691139128@qq.com>
2026-05-19 14:30:01 +0800

    feat:修改了单例应用的初始化参数 * 2

--- src/SingletonApplication/SingletonApplication.cpp
+++ src/SingletonApplication/SingletonApplication.cpp
@@ -15,6 +15,6 @@ auto SingletonApplication::init() noexcept -> void
     m_shareMemory.setKey(m_processID);
     if (!m_shareMemory.create(1, QSharedMemory::ReadWrite))
     {
-        qFatal(tr("当前应用已在系统中运行").toUtf8().constData());
+        qFatal("%s", tr("当前应用已在系统中运行").toUtf8().constData());
     }
 }
