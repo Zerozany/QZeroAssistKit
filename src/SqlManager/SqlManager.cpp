ce600cadbea5bd96d8b06f4692dc8603e1abb4d4
Zerozany <691139128@qq.com>
2026-05-28 20:48:55 +0800

    feat:添加了sql对象类 * 3

--- src/SqlManager/SqlManager.cpp
+++ src/SqlManager/SqlManager.cpp
@@ -1,11 +1,62 @@
 #include "SqlManager.h"
 
+constexpr const char* SqlDriverName{"QSQLITE"};
+
 auto SqlManager::instance(QObject* _parent) -> SqlManager*
 {
     static SqlManager* sqlManager{new SqlManager{_parent}};
     return sqlManager;
 }
 
-SqlManager::SqlManager(QObject* _parent)
+SqlManager::~SqlManager() noexcept
 {
+    for (auto& [_dataBaseName, _dataBase] : m_databasesList.toStdMap())
+    {
+        if (!_dataBase.isOpen())
+        {
+            continue;
+        }
+        _dataBase.close();
+        QSqlDatabase::removeDatabase(_dataBaseName);
+    }
+}
+
+SqlManager::SqlManager(QObject* _parent) : QObject{_parent}
+{
+    std::invoke(&SqlManager::connectSignal2Slot, this);
+}
+
+QString SqlManager::dataBaseName() const
+{
+    return m_dataBaseName;
+}
+
+void SqlManager::setDatabaseName(const QString& _dataBaseName)
+{
+    if (m_dataBaseName == _dataBaseName)
+    {
+        return;
+    }
+    m_dataBaseName = _dataBaseName;
+    Q_EMIT this->databaseNameChanged();
+}
+
+auto SqlManager::connectSignal2Slot() -> void
+{
+    connect(this, &SqlManager::databaseNameChanged, this, &SqlManager::onDatabaseNameChanged);
+}
+
+void SqlManager::onDatabaseNameChanged()
+{
+    if (QSqlDatabase::contains(this->dataBaseName()))
+    {
+        return;
+    }
+    QSqlDatabase qSqlDatabase{QSqlDatabase::addDatabase(SqlDriverName, this->dataBaseName().chopped(3))};
+    qSqlDatabase.setDatabaseName(this->dataBaseName());
+    if (!qSqlDatabase.open())
+    {
+        qWarning() << "Database not open:" << this->dataBaseName();
+    }
+    m_databasesList.insert(this->dataBaseName(), QSqlDatabase::addDatabase(SqlDriverName, this->dataBaseName().chopped(3)));
 }
--- src/SqlManager/SqlManager.h
+++ src/SqlManager/SqlManager.h
@@ -1,16 +1,65 @@
 _Pragma("once");
 #include <QObject>
+#include <QSqlDatabase>
+#include <QSqlQuery>
 
 class SqlManager : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString dataBaseName READ dataBaseName WRITE setDatabaseName NOTIFY databaseNameChanged)
 public:
     static auto instance(QObject* _parent = nullptr) -> SqlManager*;
 
-    ~SqlManager() noexcept = default;
+    ~SqlManager() noexcept;
 
     Q_DISABLE_COPY_MOVE(SqlManager)
+public:
+    QString dataBaseName() const;
+    void    setDatabaseName(const QString& _dataBaseName);
+
+public:
+    template <typename ReturnType>
+    auto executeSql(const QString& _dataBaseName, const QString& _sqlCode) -> ReturnType;
+
+private:
+    auto connectSignal2Slot() -> void;
 
 protected:
     explicit(true) SqlManager(QObject* _parent = nullptr);
+
+Q_SIGNALS:
+    void databaseNameChanged();
+
+private Q_SLOTS:
+    void onDatabaseNameChanged();
+
+private:
+    QMap<QString, QSqlDatabase> m_databasesList{};
+    QString                     m_dataBaseName{};
 };
+
+template <typename ReturnType>
+inline auto SqlManager::executeSql(const QString& _dataBaseName, const QString& _sqlCode) -> ReturnType
+{
+    if (!m_databasesList.contains(_dataBaseName))
+    {
+        qWarning() << "Database not found:" << _dataBaseName;
+        return ReturnType{};
+    }
+    QSqlDatabase dataBase{m_databasesList.value(_dataBaseName)};
+    if (!dataBase.isOpen() && !dataBase.open())
+    {
+        qWarning() << "Cannot open database:" << _dataBaseName;
+        return ReturnType{};
+    }
+    QSqlQuery query{dataBase};
+    if (!query.exec(_sqlCode))
+    {
+        qWarning() << "Execute sqlCode failed:" << _sqlCode;
+    }
+    if constexpr (std::is_void_v<ReturnType>)
+    {
+        return ReturnType{};
+    }
+    return query;
+}
